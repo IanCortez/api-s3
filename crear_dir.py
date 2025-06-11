0a1e3c2a6f09fa1e094ff505edd34e0c82b010ab
IanCortez <85956402+IanCortez@users.noreply.github.com>
2025-06-10 23:06:19 -0500

    Update crear_dir.py

--- crear_dir.py
+++ crear_dir.py
@@ -4,10 +4,11 @@ def lambda_handler(event, context):
     # Entrada (json)
     nombre_bucket = event['body']['bucket']
     dir_name = event['body']['directory']
+    dir_name = dir_name + '/'
     
     # Proceso    
     s3 = boto3.client('s3')
-    s3.put_object(Bucket=nombre_bucket, Key={dir_name+'/'})
+    s3.put_object(Bucket=nombre_bucket, Key={dir_name})
     
     return {
         'statusCode': 200,
